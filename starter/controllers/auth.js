5c7e9ce190d81490ed15db0850845ad67942b383
ujangaja <ujangaja@gmail.com>
2021-11-20 16:46:22 +0700

    #section 9: Jobs API | 175. Error Checking - Controller Example

--- starter/controllers/auth.js
+++ starter/controllers/auth.js
@@ -1,9 +1,14 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors');
 
 const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    throw new BadRequestError('Please provide name, email and password');
+  }
   const user = await User.create({ ...req.body });
-  console.log(user);
+
   res.status(StatusCodes.CREATED).json({ user });
 };
 const login = async (req, res) => {
