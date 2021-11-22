d8b4b868169167a2854eca104ec77d36d57a5451
ujangaja <ujangaja@gmail.com>
2021-11-22 15:51:08 +0700

    #section 9: Jobs API | 186. Alternative Code

--- starter/middleware/authentication.js
+++ starter/middleware/authentication.js
@@ -13,6 +13,8 @@ const auth = (req, res, next) => {
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     // attach the user to the job routes
+    // const user = User.findById(payload.id).select('-password');
+    // req.user = user;
     req.user = { userId: payload.userId, name: payload.name };
     next();
   } catch (error) {
