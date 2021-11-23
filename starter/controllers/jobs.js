0e4b1a033da109fd257aa743cd8a51c0fd560aa2
ujangaja <ujangaja@gmail.com>
2021-11-24 05:11:45 +0700

    #section 9: Jobs API | 192. Update Job

--- starter/controllers/jobs.js
+++ starter/controllers/jobs.js
@@ -24,7 +24,23 @@ const createJob = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ job });
 };
 const updateJob = async (req, res) => {
-  res.send('update job');
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  if (company === '' || position === '') {
+    throw new BadRequestError('Company or Position fields cannot be empty');
+  }
+  const job = await Job.findByIdAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  if (!job) {
+    throw new NotFoundError(`No Job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 const deleteJob = async (req, res) => {
   res.send('delete jobs');
