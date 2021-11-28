6886c2dc6485fa82c6d24eca2a010547fb39631b
ujangaja <ujangaja@gmail.com>
2021-11-28 20:47:28 +0700

    202. Deploy to Heroku

--- starter/app.js
+++ starter/app.js
@@ -34,6 +34,10 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
