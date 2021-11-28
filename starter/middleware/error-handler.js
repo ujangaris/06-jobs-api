d2a34bad9bf1b3476412cbbdec25f7af303cd4bf
ujangaja <ujangaja@gmail.com>
2021-11-28 13:49:39 +0700

    #section 9: Jobs API | 195. Custom Error

--- starter/middleware/error-handler.js
+++ starter/middleware/error-handler.js
@@ -6,9 +6,9 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || 'Something went wrong try again',
   };
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
-  }
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(
       err.keyValue
